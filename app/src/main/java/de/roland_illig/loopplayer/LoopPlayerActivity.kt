765f25595d9c79485b9b3f5b7312d51df372387d
Roland Illig <roland.illig@gmx.de>
2017-11-18 18:45:19 +0100

    The layout basically works, but no sound yet

--- app/src/main/java/de/roland_illig/loopplayer/LoopPlayerActivity.kt
+++ app/src/main/java/de/roland_illig/loopplayer/LoopPlayerActivity.kt
@@ -1,12 +1,31 @@
 package de.roland_illig.loopplayer
 
 import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
 
 class LoopPlayerActivity : Activity() {
+
+    val cuePoints = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loop_player)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
+
+    override fun onResume() {
+        super.onResume()
+        cuePoints().adapter = ArrayAdapter<String>(this, R.layout.string_list_item, cuePoints)
+    }
+
+    fun onCuePointPressed(view: View) {
+        (cuePoints().adapter as ArrayAdapter<String>).add(Math.random().toString())
+    }
+
+    fun cuePoints() = findViewById<ListView>(R.id.cuePointsList)
 }
