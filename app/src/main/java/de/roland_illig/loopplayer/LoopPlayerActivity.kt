a16b280bf2e1dab60027ec5000bf0c0eec4e7e5d
Roland Illig <roland.illig@gmx.de>
2017-11-18 17:16:19 +0100

    init

---
+++ app/src/main/java/de/roland_illig/loopplayer/LoopPlayerActivity.kt
@@ -0,0 +1,12 @@
+package de.roland_illig.loopplayer
+
+import android.app.Activity
+import android.os.Bundle
+
+class LoopPlayerActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loop_player)
+    }
+}
