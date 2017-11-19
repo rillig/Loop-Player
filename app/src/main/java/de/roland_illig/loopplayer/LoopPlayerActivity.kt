7bb434b6a0421f0b5b62086ca07e10aabb2e8ffd
Roland Illig <roland.illig@gmx.de>
2017-11-19 05:33:50 +0100

    Opening audio files works

--- app/src/main/java/de/roland_illig/loopplayer/LoopPlayerActivity.kt
+++ app/src/main/java/de/roland_illig/loopplayer/LoopPlayerActivity.kt
@@ -1,16 +1,22 @@
 package de.roland_illig.loopplayer
 
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 
 
 class LoopPlayerActivity : Activity() {
 
-    val cuePoints = ArrayList<String>()
+    private val openRequestCode = 1
+    private val cuePoints = ArrayList<String>()
+    private lateinit var audioFile: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +29,41 @@ class LoopPlayerActivity : Activity() {
         cuePoints().adapter = ArrayAdapter<String>(this, R.layout.string_list_item, cuePoints)
     }
 
+    fun onOpenPressed(view: View) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "audio/*"
+        startActivityForResult(intent, openRequestCode)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                         resultData: Intent?) {
+
+        if (requestCode == openRequestCode && resultCode == Activity.RESULT_OK) {
+            if (resultData != null) {
+                audioFile = resultData.data
+                playAudio()
+            }
+        }
+    }
+
     fun onCuePointPressed(view: View) {
         (cuePoints().adapter as ArrayAdapter<String>).add(Math.random().toString())
     }
 
-    fun cuePoints() = findViewById<ListView>(R.id.cuePointsList)
+    fun playAudio() {
+        try {
+            val assetFileDescriptor = contentResolver.openAssetFileDescriptor(audioFile, "r")
+            val fileDescriptor = assetFileDescriptor.fileDescriptor
+
+            val mp = MediaPlayer()
+            mp.setDataSource(fileDescriptor)
+            mp.prepare()
+            mp.start()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Cannot play audio: " + e, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun cuePoints() = findViewById<ListView>(R.id.cuePointsList)
 }
